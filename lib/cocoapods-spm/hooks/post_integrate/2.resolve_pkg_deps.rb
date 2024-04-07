49bc1084cd3abcc7b1d66e903101a36f9483486f
Thuyen Trinh <trinhngocthuyen@gmail.com>
2024-04-07 21:49:27 +0700

    Trigger resolve package dependencies

---
+++ lib/cocoapods-spm/hooks/post_integrate/2.resolve_pkg_deps.rb
@@ -0,0 +1,29 @@
+require "cocoapods-spm/hooks/base"
+require "cocoapods-spm/installer/analyzer"
+
+module Pod
+  module SPM
+    class Hook
+      class ResolvePkgDeps < Hook
+        def run
+          return unless @spm_analyzer.spm_pkgs
+
+          xcodebuild_resolve_package_deps
+        end
+
+        private
+
+        def xcodebuild_resolve_package_deps
+          workspace = podfile.defined_in_file.parent.glob("*.xcworkspace").first
+          scheme = workspace.parent.glob("*.xcodeproj/**/*.xcscheme").first.basename(".xcscheme")
+          system([
+            "xcodebuild",
+            "-resolvePackageDependencies",
+            "-workspace", workspace.shellescape,
+            "-scheme", scheme.shellescape,
+          ].join(" "))
+        end
+      end
+    end
+  end
+end
