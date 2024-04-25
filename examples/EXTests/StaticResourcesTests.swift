0409dabe9961ae83894987f276aac3c10a4b8e75
Thuyen Trinh <trinhngocthuyen@gmail.com>
2024-04-25 10:30:24 +0700

    CHORE: Update SnapKit to have xcprivacy resources

--- examples/EXTests/StaticResourcesTests.swift
+++ examples/EXTests/StaticResourcesTests.swift
@@ -4,6 +4,7 @@ final class StaticResourcesTests: XCTestCase {
   func testResourcesCopiedToMainBundle() {
     expectFiles(ofType: "trace", inDir: "DebugKit_DebugKit.bundle")
     expectFiles(ofType: "png", inDir: "GoogleMaps_GoogleMapsTarget.bundle/GoogleMaps.bundle")
+    expectFiles(ofType: "xcprivacy", inDir: "SnapKit_SnapKit.bundle")
   }
 
   private func expectFiles(
