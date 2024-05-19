722ebc5f912172fdd0e2417281db007fb99ddcd3
Thuyen Trinh <trinhngocthuyen@gmail.com>
2024-05-19 11:29:47 +0700

    CHORE: Update prebuilt_macros.sh

--- examples/scripts/prebuilt_macros.sh
+++ examples/scripts/prebuilt_macros.sh
@@ -4,5 +4,6 @@ set -e
 if [[ $1 == zip ]]; then
     find .spm.pods/* -type d -name ".prebuilt" -exec zip -r cache/prebuilt_macros.zip {} \;
 elif [[ $1 == unzip ]]; then
-    unzip -o cache/prebuilt_macros.zip
+    [[ ${CI} ]] && ARGS=(o) || ARGS=(n)
+    unzip -${ARGS} cache/prebuilt_macros.zip
 fi
