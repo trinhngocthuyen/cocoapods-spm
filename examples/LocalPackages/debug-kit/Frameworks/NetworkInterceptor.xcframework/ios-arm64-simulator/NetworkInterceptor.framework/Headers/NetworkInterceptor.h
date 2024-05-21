4d088c423b4c5d7a088a642095b45fd5cdcb2dd5
Thuyen Trinh <trinhngocthuyen@gmail.com>
2024-05-21 20:49:12 +0700

    FIX: Binary name with xcframework

---
+++ examples/LocalPackages/debug-kit/Frameworks/NetworkInterceptor.xcframework/ios-arm64-simulator/NetworkInterceptor.framework/Headers/NetworkInterceptor.h
@@ -0,0 +1,18 @@
+//
+//  NetworkInterceptor.h
+//  NetworkInterceptor
+//
+//  Created by Thuyen Trinh on 21/05/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkInterceptor.
+FOUNDATION_EXPORT double NetworkInterceptorVersionNumber;
+
+//! Project version string for NetworkInterceptor.
+FOUNDATION_EXPORT const unsigned char NetworkInterceptorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkInterceptor/PublicHeader.h>
+
+
