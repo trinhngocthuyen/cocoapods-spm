0f2fa5a152dec05d1e32ef64587105e66862bc3a
wangzhizhou <wangzhizhou@bytedance.com>
2024-12-03 19:21:23 +0800

    bugfix: .download not exist and bundle exec pod install failed

--- lib/cocoapods-spm/helpers/io.rb
+++ lib/cocoapods-spm/helpers/io.rb
@@ -6,6 +6,7 @@ module Pod
 
       src = Pathname.new(src) unless src.is_a?(Pathname)
       dst = Pathname.new(dst) unless dst.is_a?(Pathname)
+      dst.dirname.mkpath unless dst.dirname.exist?
       dst.delete if dst.exist?
       File.symlink(src.absolute? ? src : src.realpath, dst)
     end
