c38aaaa6dff398cad041ed2e4f71bfd2a3790416
Thuyen Trinh (Chris) <trinhngocthuyen@gmail.com>
2025-06-27 09:36:10 +0700

    FEAT: Validate pkg contains products (#165)

--- lib/cocoapods-spm/resolver/validator.rb
+++ lib/cocoapods-spm/resolver/validator.rb
@@ -4,14 +4,27 @@ module Pod
       def initialize(aggregate_targets, result)
         @aggregate_targets = aggregate_targets
         @spm_pkgs = result.spm_pkgs
+        @project_pkgs = result.project_pkgs
         @spm_dependencies_by_target = result.spm_dependencies_by_target
       end
 
       def validate!
         verify_no_missing_pkgs
+        verify_products_exist
       end
 
       private
+
+      def verify_products_exist
+        @spm_dependencies_by_target.values.flatten.uniq.each do |d|
+          next if @project_pkgs.pkg_desc_of(d.pkg.name).products.any? { |p| p.name == d.product }
+
+          raise Informative, <<~DESC
+            There was an invalid dependency in Podfile or podspecs.
+            Package `#{d.pkg.name}` does not contain product `#{d.product}`
+          DESC
+        end
+      end
 
       def dependents_of_pkg(name)
         @specs ||= @aggregate_targets.flat_map(&:specs).uniq
